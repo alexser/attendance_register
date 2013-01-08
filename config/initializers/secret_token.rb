d734e2c2a5eedd5d7ce370d78ac308bc5604650b
Alex Serdyuk <alex.serdyuk@jedo.me>
2013-01-08 18:27:52 +1100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AttendanceRegister::Application.config.secret_token = 'd7a07e5e52cb183c3978c5fe29144879fea9e46818b50e0f31cd439b16c7ebaee557261e3607692876264e21a7bbef839040912f7b85e67ec0a1eaf20633cb7f'
